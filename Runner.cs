2b09f96dab9f8c689fbf86e5d2ba0b8a8784f49b
Joseph Verron <joseph.verron@gmail.com>
2020-12-18 00:24:34 +0100

    first commit

---
+++ Runner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace dismodos
+{
+    internal static class Runner
+    {
+        private static void Main(string[] args)
+        {
+            var q = new Queue<string>();
+            q.Enqueue("Joseph");
+            q.Enqueue("Verron");
+            q.Enqueue("Louise");
+
+
+            var thread = new Thread(QueueReading);
+            thread.Start(q);
+
+            Console.WriteLine("Whatever");
+            
+            q.Enqueue("Pascal");
+            q.Enqueue("Isabelle");
+            q.Enqueue("Munoz");
+            
+            Console.ReadLine();
+            thread.Interrupt();
+        }
+
+        private static void QueueReading(object obj)
+        {
+            if (!(obj is Queue<string> queue)) 
+                return;
+            
+            Console.WriteLine("Hello, World!");
+            while (queue.Count > 0)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Hello, " + queue.Dequeue());
+            }
+        }
+    }
+}
